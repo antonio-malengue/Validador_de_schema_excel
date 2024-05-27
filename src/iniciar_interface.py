224675c5c40246c79c15e84b089acf5b7a17d5f1
Malengue <antoniocarlosmalengue@gmail.com>
2024-05-27 16:39:20 +0100

    feat: nova actualização

--- src/iniciar_interface.py
+++ src/iniciar_interface.py
@@ -5,7 +5,7 @@ import subprocess
 def executar_interface():
     #mata o processo antes de rodar a interface
     matar_processo()
-    # Executar os testes com pytest
+    # Executar os testes com pytest...
     comando = "streamlit run src/app.py"
     #subprocess.run(comando, shell=True)
     subprocess.run(["streamlit", "run", "src/app.py"])
